607b1a87077f92fcb92161245b2ed44a59403115
chanpyaung <chanpyaeaung.mr@gmail.com>
2023-02-11 14:23:14 +0800

    init project

---
+++ app/src/main/java/com/chanaung/waveformeditor/views/WaveFormView.kt
@@ -0,0 +1,17 @@
+package com.chanaung.waveformeditor.views
+
+import android.content.Context
+import android.graphics.Canvas
+import android.util.AttributeSet
+import android.view.View
+
+class WaveFormView(context: Context, attrs: AttributeSet) : View(context, attrs) {
+
+    override fun draw(canvas: Canvas?) {
+        super.draw(canvas)
+        canvas?.apply {
+
+        }
+    }
+
+}
